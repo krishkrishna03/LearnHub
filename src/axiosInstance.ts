f67b50e4f675bc759618d0e7f721cedcf27d27e5
patnala sri krishna sai <96357392+krishkrishna03@users.noreply.github.com>
2025-07-20 01:26:19 +0530

    Create axiosInstance.ts

---
+++ src/axiosInstance.ts
@@ -0,0 +1,17 @@
+// src/axiosInstance.ts
+
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'https://learnhub-869c.onrender.com/api',
+});
+
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => Promise.reject(error));
+
+export default instance;
